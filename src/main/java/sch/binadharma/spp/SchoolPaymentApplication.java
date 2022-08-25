67e734a8025412401198474fa53e5b176b17160e
suharbramar <suhar_bramar@yahoo.com>
2022-08-25 23:36:32 +0700

    fix develop

--- src/main/java/sch/binadharma/spp/SchoolPaymentApplication.java
+++ src/main/java/sch/binadharma/spp/SchoolPaymentApplication.java
@@ -2,21 +2,14 @@ package sch.binadharma.spp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-<<<<<<< HEAD:src/main/java/io/bramcode/movie/moviecategoryservices/MovieCategoryServicesApplication.java
-@EnableEurekaClient
-@EnableCaching
-public class MovieCategoryServicesApplication {
-=======
 //@EnableEurekaClient
 public class SchoolPaymentApplication {
->>>>>>> c977bf7f2ce2e55e0f355cc0c1034b36defbce8c:src/main/java/sch/binadharma/spp/SchoolPaymentApplication.java
 
 	//singleton
 	//rest template map to this one instance
